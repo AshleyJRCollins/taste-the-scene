3bbdc12b0ce963bf974e568072f9825b1b1dd732
Ashley Collins <ashleyjohnroycollins@gmail.com>
2021-02-10 21:39:08 +0100

    orders index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,9 +17,16 @@ def index
     redirect_to order_path(@order)
   end
 
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+
+    redirect_to orders_path
+  end
+
 private
 
   def order_params
-    params.require(:order).permit(:name, :option, :number_of_boxes, :address, :telephone_number, :email)
+    params.require(:order).permit(:name, :option, :number_of_boxes, :address, :telephone_number, :email, :dietary_requirements)
   end
 end
