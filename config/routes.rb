e5d1c7f7b74182c2d0360f2cbd85722b6c812991
Ashley Collins <ashleyjohnroycollins@gmail.com>
2021-08-30 21:17:16 +0200

    events-page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'events', to: 'pages#events'
+  get 'confirmation', to: 'pages#confirmation'
   resources :orders, only: %i[index show new create destroy]
 end
